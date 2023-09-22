17088bf60ea434afc8ecf6bce3b51645f06ce9d0
Gigih <gigihramadhan20@gmail.com>
2023-09-22 16:54:00 +0700

    optional pass semua dan eslint pass

--- src/Server.js
+++ src/Server.js
@@ -1,14 +1,14 @@
-const Hapi = require("@hapi/hapi");
-const routes = require("./Route");
+const Hapi = require('@hapi/hapi');
+const routes = require('./Route');
 // kriteria 1 port 9000
 // kriteria 2 npm run start 'start': 'node src/Server.js'
 const init = async () => {
   const server = Hapi.server({
     port: 9000,
-    host: "localhost",
+    host: 'localhost',
     routes: {
       cors: {
-        origin: ["*"],
+        origin: ['*'],
       },
     },
   });
